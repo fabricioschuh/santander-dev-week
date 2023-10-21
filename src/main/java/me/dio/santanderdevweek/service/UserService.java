7da548bceb965ff3cbb412c5a61b707753015b46
Fabricio Schuh <fabricio.schuh@sap.com>
2023-10-21 13:58:04 -0300

    adding service layer

---
+++ src/main/java/me/dio/santanderdevweek/service/UserService.java
@@ -0,0 +1,13 @@
+package me.dio.santanderdevweek.service;
+
+import me.dio.santanderdevweek.domain.model.User;
+
+public interface UserService {
+
+    User findById(Long id);
+
+    User create(User userToCreate);
+
+
+
+}
---
+++ src/main/java/me/dio/santanderdevweek/service/impl/UserServiceImpl.java
@@ -0,0 +1,30 @@
+package me.dio.santanderdevweek.service.impl;
+
+import lombok.AllArgsConstructor;
+import me.dio.santanderdevweek.domain.model.User;
+import me.dio.santanderdevweek.repository.UserRepository;
+import me.dio.santanderdevweek.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+@AllArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        if (userToCreate.getId() != null && userRepository.existsById(userToCreate.getId())) {
+            throw new IllegalArgumentException("The user ID already exists.");
+        }
+
+        return userRepository.save(userToCreate);
+    }
+}
